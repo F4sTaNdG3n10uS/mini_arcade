97ff8b66f1e0a84f5103fd271112115c6e9423ad
PopyPoilue <wilnachongchareun@gmail.com>
2026-01-22 11:24:23 +0100

    keklake

--- jeux/morpion.py
+++ jeux/morpion.py
@@ -1,11 +1,20 @@
-import pygame
-pygame.init()
-screen = pygame.display.set_mode((800,700))
-pygame.display.set_caption("Morpion")
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-pygame.quit()
+import pygame 
+
+def start():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 700))
+    pygame.display.set_caption("Morpion")
+
+    clock = pygame.time.Clock()
+    running = True
+    while running:  
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))  # fond noir
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
